4f685fac64b122e94c1f1f75382849a1b996dd4e
Yogesh <syogeshkumar1411@gmail.com>
2022-11-21 09:24:14 +0530

    20-11-2022

--- 002pseudo_char_driver/main.c
+++ 002pseudo_char_driver/main.c
@@ -1,8 +1,14 @@
 #include<linux/module.h>
 #include<linux/init.h>
 #include<linux/fs.h>
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/k_dev_t.h>
 
 #define DEV_MEM_SIZE 512  // limit of this driver
+
+#undef pr_fmt 
+#define pr_fmt(fmt) "%s:" fmt,__func__
 
 /*pseudo device's memory*/
 char device_buffer[DEV_MEM_SIZE];
@@ -10,13 +16,91 @@ char device_buffer[DEV_MEM_SIZE];
 /*This holds the device number*/
 dev_t device_number;
 
-/* Cdev
+/* Cdev variable */
+struct cdev pcd_cdev;
+
+
+
+
+loff_t pcd_lseek(struct file *filp, loff_t off, int whence)
+{
+	pr_info("lseek requested\n");
+
+        return 0;
+}
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
+{
+	pr_info("read requested for %zu bytes \n",count);
+
+        return 0;
+}
+ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
+{
+	pr_info("write requested for %zu byted \n",count);
+
+
+        return 0;
+}
+int pcd_open(struct inode *inode, struct file *filp)
+{
+	pr_info("open was successful\n");
+
+        return 0;
+}
+int pcd_release(struct inode *inode, struct file *filp)
+{
+	pr_info("release was successful\n");
+
+        return 0;
+}
+
+
+
+
+
+/* file operations of the driver */
+
+struct file_operations pcd_fops = 
+{
+	.open = pcd_open,
+	.write = pcd_write,
+	.read = pcd_read,
+	.close = pcd_release,
+	.llseek = pcd_lseek,
+	.owner = THIS_MODULE
+};
+
+struct class *class_pcd;
+
+struct device *device_pcd;
 
 
 static int __init pcd_driver_init(void){
 
 	/*1. Dynamically allocate a device number*/
-	alloc_chrdev_region(&device_number,0,7,"pcd");
+	alloc_chrdev_region(&device_number,0,7,"pcd_device");
+
+
+	pr_info("Device number <major>:<minor> = %d:%d\n",MAJOR(device_number),MINOR(device_number));
+
+	/*2. Initialize the cdev structure with fops*/
+	cdev_init(&pcd_cdev,&pcd_fops);
+
+	/*3. Register cdev device (cdev strucure) with VFS*/
+	pcd_cdev.owner=THIS_MODULE;
+	cdev_add(&pcd_cdev,device_number,1);
+	
+
+	/*4. Creating device file */
+	/*Create device class under /sys/class*/
+	class_pcd = class_create(THIS_MODULE,"pcd_class");
+
+	/*Populate the sysfs with device information*/
+	device_pcd = device_create(class_pcd,NULL,device_number,NULL,"pcd");
+
+
+	pr_info?("Module init was successful\n");
+
 
 	return 0;
 }
