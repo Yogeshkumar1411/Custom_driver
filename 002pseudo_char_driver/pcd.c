ba88d68260a2e2687fadbcd6b934defcd58e03cf
Yogesh <syogeshkumar1411@gmail.com>
2022-11-22 12:08:20 +0530

    final_pcd

--- 002pseudo_char_driver/pcd.c
+++ 002pseudo_char_driver/pcd.c
@@ -82,6 +82,7 @@ ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_p
 	/*
 	 * update the current file position
 	 * */
+
 	*f_pos+=count;
 
 	pr_info("Number of bytes successfully read = %zu\n",count);
@@ -94,7 +95,7 @@ ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_p
 }
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
 {
-	pr_info("write requested for %zu byted \n",count);
+	pr_info("write requested for %zu bytes \n",count);
 	pr_info("current file position = %lld\n",*f_pos);
 
 	/*
@@ -103,8 +104,10 @@ ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff
 	if((*f_pos + count)>DEV_MEM_SIZE)
 		count = DEV_MEM_SIZE - *f_pos;
 	
-	if(!count)
+	if(!count){
+		pr_err("No space left on the device\n");
 		return -ENOMEM;
+	}
 
 	/*
 	 * copt from user
@@ -162,33 +165,87 @@ struct device *device_pcd;
 
 static int __init pcd_driver_init(void){
 
-	/*1. Dynamically allocate a device number*/
-	alloc_chrdev_region(&device_number,0,7,"pcd_device");
+	int ret;
 
+	/*
+	 * 1. Dynamically allocate a device number
+	 * */
+	ret = alloc_chrdev_region(&device_number,0,7,"pcd_device");
+	if(ret < 0){
+		pr_err("Alloc chrdev failed\n");
+		goto out;
+	}
 
 	pr_info("Device number <major>:<minor> = %d:%d\n",MAJOR(device_number),MINOR(device_number));
 
-	/*2. Initialize the cdev structure with fops*/
+	/*
+	 * 2. Initialize the cdev structure with fops
+	 * */
 	cdev_init(&pcd_cdev,&pcd_fops);
 
-	/*3. Register cdev device (cdev strucure) with VFS*/
+	/*
+	 * 3. Register cdev device (cdev strucure) with VFS
+	 * */
 	pcd_cdev.owner=THIS_MODULE;
-	cdev_add(&pcd_cdev,device_number,1);
-	
+	ret = cdev_add(&pcd_cdev,device_number,1);
+	if(ret < 0){
+		pr_err("cdev_add failed\n");
+		goto unreg_chrdev;
+	}
 
-	/*4. Creating device file */
-	/*Create device class under /sys/class*/
+	/*
+	 * 4. Creating device file 
+	 * */
+	/*
+	 * Create device class under /sys/class
+	 * */
 	class_pcd = class_create(THIS_MODULE,"pcd_class");
+	/*
+	 * class create returns poniter. So while failing-
+	 * it returns pointer to negative value. So the macro-
+	 * IS_ERR is used
+	 * */
+	if(IS_ERR(class_pcd))
+	{
+		pr_err("Class creation failed\n");
+		/*
+		 * PTR_ERR() converts pointer to error code(int)
+		 * ERR_PTR() converts error code (int) to pointer
+		 * */
+		ret = PTR_ERR(class_pcd);
+		goto cdev_del;
+	}
 
-	/*Populate the sysfs with device information*/
+
+	/*
+	 * Populate the sysfs with device information
+	 * */
 	device_pcd = device_create(class_pcd,NULL,device_number,NULL,"pcd");
-
+	if(IS_ERR(device_create))
+	{
+		pr_err("Device create failed\n");
+		ret = PTR_ERR(device_pcd);
+		goto class_del;
+	}
 
 	pr_info("Module init was successful\n");
 
 
 	return 0;
+
+class_del:
+	class_destroy(class_pcd);
+cdev_del:
+	cdev_del(&pcd_cdev);
+unreg_chrdev:
+	unregister_chrdev_region(device_number,1);
+out:
+	pr_info("Module insertion failed\n");
+	return ret;
+
 }
+
+
 
 static void __exit pcd_driver_cleanup(void){
 	
