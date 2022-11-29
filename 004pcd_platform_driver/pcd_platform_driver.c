65b047a35da81fe4f667046a7ae9e949bbf9cd9d
Yogesh <syogeshkumar1411@gmail.com>
2022-11-29 13:06:50 +0530

    probe funcion

--- 004pcd_platform_driver/pcd_platform_driver.c
+++ 004pcd_platform_driver/pcd_platform_driver.c
@@ -2,6 +2,9 @@
 #include<linux/fs.h>
 #include<linux/device.h>
 #include<linux/platform_device.h>
+#include<linux/slab.h>
+#include<linux/cdev.h>
+
 
 #include"platform.h"
 
@@ -9,6 +12,31 @@
 
 #undef pr_fmt
 #define pr_fmt(fmt) "%s : " fmt,__func__
+
+
+/*Device private data strucure*/
+struct pcdev_private_data
+{
+	struct pcdev_platform_data pdata;
+	char *buffer;
+	dev_t dev_num;
+	struct cdev cdev;
+};
+
+/*Driver private data structure */
+struct pcdrv_private_data
+{
+	int total_devices;
+	dev_t device_num_base;
+	struct class *class_pcd;
+	struct device *device_pcd;
+
+};
+
+/*for the driver we are going to create one variable globally, but for the device we will create dynamically, whenever our probve function gets called*/
+
+struct pcdrv_private_data pcdrv_data;
+
 
 int check_permission(int dev_perm, int acc_mode)
 {
@@ -71,16 +99,106 @@ struct file_operations pcd_fops = {
 /*gets called when the device is removed from the system*/
 int pcd_platform_driver_remove(struct platform_device *pdev)
 {
+	/*1. Remove a device that was created wih device_create*/
+
+	/*2. Remove a cdev entry from the system*/
+
+	/*3. Free the memory held by the device*/
+	
+
 	pr_info("A device is removed\n");
 	return 0;
-}
+} 
 
-/*gets called when matched plaform device is found*/
-int pcd_platform_driver_probe(struct platform_device *pdev) 
+/*gets called when matched plaform device is found.
+ * *pdev is the pointer to matched data */
+int pcd_platform_driver_probe(struct platform_device *pdev)  
 {
+
+	/*in this function we will use driver private data sturcture, for that we have declared the variable globally. In 1st step we will get the
+	 * information of device details which had been done in 'pcd_device_setup.c' file. There we have registered the device details, so it will
+	 *  fetched into the 'platform_device' structure in the field of 'dev'. And then by exracting the that dev field we will get the information
+	 *  of devices*/
+
+	int ret;
+	struct pcdev_private_data *dev_data;
+	
+	struct pcdev_platform_data *pdata;
+
 	pr_info("A device is detected\n");
+
+        /*1. Get the platform data (extracting)
+	 * pdata = pdev->dev.plaform_data;
+	 * another method of extracting below 
+	 * And, it returns void pointer and need to typecast  */
+	pdata = (struct pcdev_platform_data*)dev_get_platdata(&pdev->dev);
+	if(!pdata){
+		pr_info("No platform data available\n");
+		ret = -EINVAL;
+		goto out;
+	}
+
+        /*2. Dynamically allocate memory for the device private data*/
+	dev_data = kzalloc(sizeof(*dev_data),GFP_KERNEL);//Doubt, already the pointer having 8 bytes then after assigning the same memory space// *pointer give will total size of structure
+       	if(!dev_data){
+ 		pr_info("No platform data available\n");
+		ret = -EINVAL;
+		goto out;
+	}
+
+	dev_data->pdata.size = pdata->size;
+	dev_data->pdata.perm = pdata->perm;
+	dev_data->pdata.serial_number = pdata->serial_number;
+
+	pr_info("Device serial number = %s\n",dev_data->pdata.serial_number);
+
+        /*3. Dynamically allocate memory for the device buffer using size
+ information form the platform data*/
+	dev_data->buffer = kzalloc(dev_data->pdata.size,GFP_KERNEL);
+	if(!dev_data->buffer){
+		pr_info("Cannot allocate memory\n");
+		ret = -ENOMEM;
+		goto dev_data_free; 
+	}
+
+
+        /*4. Get the device number */
+	dev_data->dev_num = pcdrv_data.device_num_base + pdev->id;
+
+        /*5. Do  cdev init and cdev add*/
+	cdev_init(&dev_data->cdev,&pcd_fops);
+
+	dev_data->cdev.owner = THIS_MODULE;
+	ret = cdev_add(&dev_data->cdev,dev_data->dev_num,1);
+	if(ret < 0){
+		pr_err("cdev add failed\n");
+		goto buffer_free;
+	}
+
+        /*6. Create device file for the detected platform device*/
+	pcdrv_data.device_pcd = device_create(pcdrv_data.class_pcd,NULL,dev_data->dev_num,NULL,"pcdev-%d",pdev->id);
+	if(IS_ERR(pcdrv_data.device_pcd)){
+		pr_err("Device create failed\n");
+		ret = PTR_ERR(pcdrv_data.device_pcd);
+		goto cdev_del;
+	}
+
+
+	
+	pr_info("The probe was successful\n");
 	
 	return 0;
+
+/*7. Error handling */
+cdev_del:
+	cdev_del(&dev_data->cdev);
+buffer_free:
+	kfree(dev_data->buffer);
+dev_data_free:
+	kfree(dev_data);
+out:
+	pr_info("Device probe failed\n");
+	return ret;
 }
 
 struct platform_driver pcd_platform_driver = {
@@ -92,9 +210,34 @@ struct platform_driver pcd_platform_driver = {
 
 };
 
-static int __init platform_driver_init(void){
+#define MAX_DEVICES 10
 
+static int __init platform_driver_init(void){
+	
+	int ret;
+
+	/*1. Dynamically allocate a device number for MAX_DEVICES*/
+	ret = alloc_chrdev_region(&pcdrv_data.device_num_base,0,MAX_DEVICES,"pcdevs");
+	if(ret < 0)
+	{
+		pr_err("Alloc chrdev failed\n");
+		return ret;
+	}
+
+
+	/*2. Create device class under /sys/class*/
+	pcdrv_data.class_pcd = class_create(THIS_MODULE,"pcd_class");
+	 if(IS_ERR(pcdrv_data.class_pcd)){
+		 pr_err("Class creation failed\n");
+		 ret = PTR_ERR(pcdrv_data.class_pcd);
+		 unregister_chrdev_region(pcdrv_data.device_num_base,MAX_DEVICES);
+		 return ret;
+	 }
+
+
+	/*3. Register a platform driver*/
 	platform_driver_register(&pcd_platform_driver);
+	 
 	pr_info("pcd platform driver loaded\n");
 
 	return 0;
@@ -104,7 +247,15 @@ static int __init platform_driver_init(void){
 
 static void __exit platform_driver_exit(void){
 
+	/*1. Unregister the platform module*/
 	platform_driver_unregister(&pcd_platform_driver);
+
+	/*2. Class destroy*/
+	class_destroy(pcdrv_data.class_pcd);
+
+	/*3. Unregister device numbers for MAX_DRVICES*/
+	unregister_chrdev_region(pcdrv_data.device_num_base,MAX_DEVICES);
+
 	pr_info("pcd platform driver unloaded\n");
 
 }
